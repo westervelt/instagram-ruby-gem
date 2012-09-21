b0d9b649699c03bd42761c519ecf59e16e123f38
Brett Westervelt <brett.westervelt@gmail.com>
2012-09-21 13:48:17 -0700

    fix for instagram API

--- lib/instagram/client/locations.rb
+++ lib/instagram/client/locations.rb
@@ -62,7 +62,7 @@ module Instagram
       # @authenticated false
       # @rate_limited true
       def location_search_foursquare(foursquare, options={})
-        response = get('locations/search', options.merge(:FOURSQUARE_V2_ID => foursquare))
+        response = get('locations/search', options.merge(:distance => 'foursquare', :foursquare_v2_id => foursquare))
         response["data"]
       end
       
